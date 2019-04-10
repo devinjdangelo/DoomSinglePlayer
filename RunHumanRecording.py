3468c6cbbc511817f83f1f57ce3b9293d5b4d503
devinjdangelo <devinjdangelo@gmail.com>
2019-04-10 11:17:07 -0400

    Add files via upload

--- RunHumanRecording.py
+++ RunHumanRecording.py
@@ -2,13 +2,14 @@ from model.Simulator import DoomSimulator
 from DefaultArgs import args
 
 args['mode'] = 'record'
-args['mem_location'] = '/media/ddangelo/sandisk/human_data_1min_d.h5'
+args['mem_location'] = '/home/ddangelo/Documents/data.h5'
 args['test_stat_file'] = 'human_test_data.csv'
 args['reset_file'] = True
 
 #Here you should place doom2.wad and your vizdoom config file
 #No spaces allowed in path due to limitation with pyoblige!
-args['doom_files_path'] = '/home/ddangelo/Documents/'
+args['episodes_per_wad'] = 11
+args['doom_files_path'] = '/home/ddangelo/Documents/doomfiles/'
 args['doom_engine'] = 'doom2.wad'
 
 
