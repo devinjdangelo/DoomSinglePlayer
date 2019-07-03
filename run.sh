03a261d3b5c690b0110cbb2d106e0425b95c75ed
Devin D'Angelo <devinjdangelo@gmail.com>
2019-07-02 21:47:19 -0400

    remove shell scripts from repo

--- run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-mpiexec -n 16 -oversubscribe --allow-run-as-root python3 -u "/home/ddangelo/Dropbox/Deep Learning/doom-keys/RunTraining.py"
--- train.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo docker run -it --runtime=nvidia --privileged --shm-size=1g -v "$PWD":"$PWD" \
--v "/home/ddangelo/Documents/Tensorflow/doom-ckpts":"/home/ddangelo/Documents/Tensorflow/doom-ckpts" \
--w "$PWD" \
-doomrl ./run.sh
