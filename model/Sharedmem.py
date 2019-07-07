c39909987f9bd7e5225f09b930e9deb77876f34d
Devin D'Angelo <devinjdangelo@gmail.com>
2019-07-07 16:54:05 -0400

    fixed shared memory allocation duplicates

--- model/Sharedmem.py
+++ model/Sharedmem.py
@@ -13,7 +13,11 @@ def SharedArray(shape,dtype=np.float32):
 			nbytes = reduce(mul,shape,1)*bytesize
 		except TypeError:
 			nbytes = shape*bytesize
-		win = MPI.Win.Allocate_shared(nbytes, bytesize, comm=comm) 
+		#nbytes /= workers
+		if rank==0:
+			win = MPI.Win.Allocate_shared(nbytes, bytesize, comm=comm) 
+		else:
+			win = MPI.Win.Allocate_shared(0, bytesize, comm=comm) 
 		buf, itemsize = win.Shared_query(0) 
 		return np.ndarray(buffer=buf, dtype=np.float32, shape=shape) 
 
